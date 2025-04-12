f2d4bc11544b31679b48b1021018ed5fb415a54d
Davide Sforza <davidesforza06@gmail.com>
2025-04-12 17:23:19 +0200

    marco angioni

--- src/search.php
+++ src/search.php
@@ -44,7 +44,6 @@
             </table>
         </div>
     </div>
-    <?php include 'shared/footer.php'?>
     <script src="assets/scripts/searchScript.js"></script>
     <?php
         $searchQuery = $_GET["searchQuery"];
@@ -52,6 +51,7 @@
                 searchClick(\''.$searchQuery.'\', true);
             </script>');
     ?>
+    <?php include 'shared/footer.php'?>
     <script src="assets/scripts/scriptMain.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
